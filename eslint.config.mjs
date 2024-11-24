571b62525152ede740e3683f9667f4851bc44bae
mjlun <jtl24@calvin.edu>
2024-11-23 20:00:27 -0500

    fixed react version not specified error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,23 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  files: ["**/*.{js,mjs,cjs,jsx}"],
+  languageOptions: {
+    globals: globals.browser,
+  },
+  plugins: {
+    js: pluginJs,
+    react: pluginReact,
+  },
+  rules: {
+    ...pluginJs.configs.recommended.rules,
+    ...pluginReact.configs.recommended.rules,
+  },
+  settings: {
+    react: {
+      version: "detect", // Automatically detect the react version
+    },
+  },
+};
